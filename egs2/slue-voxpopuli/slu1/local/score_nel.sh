bcce70f07e93a61dfce8229657e838cb979071c2
Ankita Pasad <ankitap@ttic.edu>
2024-03-02 12:40:56 -0600

    polish data prep and eval

--- egs2/slue-voxpopuli/slu1/local/score_nel.sh
+++ egs2/slue-voxpopuli/slu1/local/score_nel.sh
@@ -1,18 +1,29 @@
-dir_name=$1
-split="devel"
+log_dir=$1 # directory to save results
+token_fn=$2 # decoded frame-level outputs
+split=$3
+tune=$4 # 1: tune, 0: don't tune
+# split="devel"
 
 if ! test -f "data/nel_gt/${split}_all_word_alignments.json"; then
     python local/prepare_nel_data.py
 fi
-python local/reformat_ctc_outputs.py --dir_name $dir_name --split $split
 
-# offset=0.0
-# python local/score_nel.py evaluate_submission --dir_name $dir_name --split $split --ms False --offset -0.0
+if [[ "$log_dir" == *"whisper"* ]]; then
+    python local/reformat_ctc_outputs.py --token_fn $token_fn --log_dir $log_dir --split $split --frame_len 2e-2
+elif [[ "$log_dir" == *"slurp"* ]]; then
+    python local/reformat_ctc_outputs.py --token_fn $token_fn --log_dir $log_dir --split $split --frame_len 0.032
+else
+    python local/reformat_ctc_outputs.py --token_fn $token_fn --log_dir $log_dir --split $split --frame_len 4e-2
+fi
 
-# uncomment the lines below to tune offset parameter
-
-for offset in $(seq -0.3 0.02 0.3); do
-    python local/score_nel.py evaluate_submission --dir_name $dir_name --split $split --ms False --offset $offset
-done
-
-python local/score_nel.py choose_best --dir_name $dir_name
+if [ "$split" = "test" ]; then
+    python local/score_nel.py eval_test --log_dir $log_dir
+elif [ "$tune" -eq 1 ]; then
+    for offset in $(seq -0.3 0.02 0.3); do
+        python local/score_nel.py evaluate_submission --log_dir $log_dir --split $split --ms False --offset $offset
+    done
+    python local/score_nel.py choose_best --log_dir $log_dir
+else
+    offset=0.0
+    python local/score_nel.py evaluate_submission --log_dir $log_dir --split $split --ms False --offset 0.0
+fi
